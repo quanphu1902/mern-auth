18b4e41992dc42aa98b6328cc41d917ad2c39bd7
quanphu1902 <88502952+quanphu1902@users.noreply.github.com>
2024-05-26 00:55:55 +0700

    fix

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,4 +1,3 @@
-
 import {useSelector} from 'react-redux'
 import { Outlet, Navigate } from 'react-router-dom'
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
