b1a88378595efc87fb89584cc1f1666c6f38414d
quanphu1902 <88502952+quanphu1902@users.noreply.github.com>
2024-05-25 22:43:56 +0700

    errors handle

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -13,3 +15,22 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
